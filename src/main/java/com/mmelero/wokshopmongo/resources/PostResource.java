c70e18681519c40a78284fc06290448aab67c22a
mmelero <mmelero@gmail.com>
2021-02-12 15:48:40 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/mmelero/wokshopmongo/resources/PostResource.java
+++ src/main/java/com/mmelero/wokshopmongo/resources/PostResource.java
@@ -1,15 +1,17 @@
 package com.mmelero.wokshopmongo.resources;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mmelero.wokshopmongo.domain.Post;
+import com.mmelero.wokshopmongo.resources.util.URL;
 import com.mmelero.wokshopmongo.services.PostService;
 @RestController
 @RequestMapping(value="/posts")
@@ -22,6 +24,14 @@ public class PostResource {
  	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body((obj));
+	}
+
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		//decodificar o parametro recebido pela url
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body((list));
 	}
 
 }
